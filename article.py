2558d6feec6149e85ad9f3762b15c7fb6b3a7bc9
DevonWright <devon.a.wright95@gmail.com>
2020-01-05 13:12:15 -0800

    class used to hold an articles details

---
+++ article.py
@@ -0,0 +1,6 @@
+class Article:
+    def __init__(self, title=None, link=None, content=None, sentiment=None):
+        self.title = title
+        self.link = link
+        self.content = content
+        self.sentiment = sentiment
